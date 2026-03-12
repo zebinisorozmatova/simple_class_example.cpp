b04de1ae9778253c50f7778bb9080446c48972d8
Zebiniso Roʻzmatova <zebinisorozmatova@gmail.com>
2026-03-12 23:46:29 +0500

    Create number_class.cpp

---
+++ number_class.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+class Number {
+public:
+    void showNumber() {
+        cout << 25 << endl;
+    }
+};
+
+int main() {
+    Number n;
+    n.showNumber();
+}
